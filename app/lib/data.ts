aa35a81cb03880411f6b188e46d223cbe9f8e834
yuki330m <yy.mn330b@gmail.com>
2023-12-23 10:59:05 +0900

    fix: nostore()読み込位置修正

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -56,8 +56,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
   try {
-    noStore();
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
